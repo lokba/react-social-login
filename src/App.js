31bff34d8a7afad615845e3b4405a4836e6e12c5
kimsanglok <evertree6031@gmail.com>
2021-08-20 20:47:44 +0900

    Feature : react-router 적용 및 프로젝트 구조 세팅 resolved :#1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RootRoute from './routes';
+
+const App = () => {
+  return (
+    <RootRoute />
+  );
+};
+
+export default App;
---
+++ src/pages/NoMatchPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatchPage = () => {
+    return (
+        <div>
+            no match page!
+        </div>
+    );
+};
+
+export default NoMatchPage;
