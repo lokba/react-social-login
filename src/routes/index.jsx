661e3d943f24a92abf93cda77801ede73149f0b8
Kimsanglok <evertree6031@gmail.com>
2021-08-31 18:24:57 +0900

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import NoMatchPage from '../pages/NoMatch';
 import GlobalStyles from '../Style/GlobalStyle';
